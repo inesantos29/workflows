25de2f52db3e5eed2813ab899d8e2deec25b3b2c
Inês dos Santos <ines.arts@hotmail.com>
2017-03-31 15:45:32 +0100

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,48 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify')
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee']
 var jsSources = [
-  'components/scripts/rclick.js',
-  'components/scripts/pixgrid.js',
-  'components/scripts/tagline.js',
-  'components/scripts/template.js'
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
 ];
-var sassSources = ['components/sass/style.scss'];
 
-gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-    .pipe(coffee({bare:true})
-      .on('error', gutil.log))
-    .pipe(gulp.dest('components/scripts'))
+var sassSources = ['components/sass/style.scss']
+
+gulp.task('coffee', function(){
+    gulp.src(coffeeSources)
+        .pipe(coffee({bare:true})
+        .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts')
+    )
 });
 
-gulp.task('js', function() {
-  gulp.src(jsSources)
-    .pipe(concat('script.js'))
-    .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+gulp.task('js', function(){
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest('builds/development/js')
+        .pipe(connect.reload())
+    )
 });
 
 gulp.task('compass', function() {
-  gulp.src(sassSources)
-    .pipe(compass({
-      sass:'components/sass',
-      image:'builds/development/images',
-      style:'expanded'
-    })
-      .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
-    .pipe(notify('Styles Compiled & Deployed!'))
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(notify('Styles Compiled & Deployed!'))
+        .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
@@ -47,5 +53,11 @@ gulp.task('watch', function(){
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
+gulp.task('connect', function(){
+    connect.server({
+        root:'builds/development/',
+        livereload:true
+    });
+});
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
